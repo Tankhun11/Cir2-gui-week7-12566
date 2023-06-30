14ef0ae9204b01ca7a60483c2f719940d88ff772
Tankhun <tankoon300950@gmail.com>
2023-06-30 09:34:29 +0700

    first app

---
+++ bin/dart_week7.dart
@@ -0,0 +1,11 @@
+void main(){
+  print("Logic operater");
+  int x = 10;
+  int y = 9;
+  bool a = true;
+  bool b =  false;
+
+  print("x==y = ${x==y}");//equal
+  print("&a|b = ${a|b}");//or
+  print("a&b = ${a&b}")//and gate
+}
